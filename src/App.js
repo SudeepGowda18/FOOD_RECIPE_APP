081a5124a883415117352feaff0c183aae53e226
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-15 22:13:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
