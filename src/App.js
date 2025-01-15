442bb33153a43712477f214ed988ca0a27a7019f
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-15 22:14:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { GiKnifeFork } from "react-icons/gi";
 
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
